c0f7c0fb4f93cae0f9286601e45ff3d5b360764f
tvargasl <126362144+tvargasl@users.noreply.github.com>
2023-06-29 17:05:38 -0500

    Add files via upload

---
+++ Actividad6/src/main/java/misproyectos/actividad6/Actividad6.java
@@ -0,0 +1,11 @@
+package misproyectos.actividad6;
+
+public class Actividad6 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal iu = new VentanaPrincipal();
+        iu.setVisible(true);
+        iu.setResizable(false);
+
+    }
+}
